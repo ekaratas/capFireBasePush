b14ba5284849c386d58ddd859333725a350484e0
Erinc Karatas <1380275+ekaratas@users.noreply.github.com>
2019-12-04 01:52:42 +0300

    v1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
+
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
+
+  bildirimListe;
 
   constructor() {}
+  async getObject() {
+    const ret = await Storage.get({ key: 'bildirimler' });
+    this.bildirimListe = JSON.parse(ret.value);
+    console.log(this.bildirimListe);
+  }
+
 
 }
